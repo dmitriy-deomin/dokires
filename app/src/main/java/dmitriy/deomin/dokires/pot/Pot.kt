04e74711e8ad9d4500cb944eb53f008691fde502
dimon <58627@bk.ru>
2017-11-29 20:59:16 +0300

    ля ля ля

--- app/src/main/java/dmitriy/deomin/dokires/pot/Pot.kt
+++ app/src/main/java/dmitriy/deomin/dokires/pot/Pot.kt
@@ -1,11 +1,17 @@
 package dmitriy.deomin.dokires.pot
 import android.annotation.SuppressLint
+import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 import android.support.v4.view.ViewPager
 import android.view.View
 import dmitriy.deomin.dokires.Main
 import dmitriy.deomin.dokires.R
+import kotlinx.android.synthetic.main.pot.*
+import org.jetbrains.anko.backgroundColor
+import org.jetbrains.anko.sdk25.coroutines.onClick
+import org.jetbrains.anko.sdk25.coroutines.onScrollChange
+import org.jetbrains.anko.support.v4.onPageChangeListener
 
 class Pot : FragmentActivity(){
 
@@ -26,12 +32,45 @@ class Pot : FragmentActivity(){
         viewpager.offscreenPageLimit = 3
 
 
+        viewpager.onPageChangeListener {
+            onPageSelected {
+               fon_butn(viewpager.currentItem)
+            }
+        }
+
+
         //если первый раз открывает то покажем оглавление
         if(Main.read_str("old_text_book_pot").length<2) {
             viewpager.currentItem = 0
         }else{
             //иначе продолжим чтение
             viewpager.currentItem = 1
+        }
+
+
+        btn_oglavlenie.onClick { viewpager.currentItem=0 }
+        btn_book_text.onClick { viewpager.currentItem=1 }
+        btn_tablica.onClick { viewpager.currentItem=2 }
+    }
+
+
+    fun fon_butn(number:Int){
+        when(number){
+            0->{
+                btn_oglavlenie.typeface = Typeface.DEFAULT_BOLD
+                btn_book_text.typeface = Typeface.DEFAULT
+                btn_tablica.typeface = Typeface.DEFAULT
+            }
+            1->{
+                btn_book_text.typeface = Typeface.DEFAULT_BOLD
+                btn_oglavlenie.typeface = Typeface.DEFAULT
+                btn_tablica.typeface = Typeface.DEFAULT
+            }
+            2->{
+                btn_tablica.typeface = Typeface.DEFAULT_BOLD
+                btn_book_text.typeface = Typeface.DEFAULT
+                btn_oglavlenie.typeface = Typeface.DEFAULT
+            }
         }
 
     }
